af3fa9e676c9236ae12361fbea53a39771059813
Baris Bilgin <brokoliweb@gmail.com>
2023-03-01 01:34:50 +0300

    install axios styled-components & delete redundant files

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -1,9 +1,1 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
-
-export const client = new ApolloClient({
-  uri: 'https://api.themoviedb.org/3/graphql',
-  cache: new InMemoryCache(),
-  headers: {
-    authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`,
-  },
-})
+// TODO
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,14 +1,1 @@
-import { gql } from '@apollo/client'
-
-export const POPULAR_MOVIES_QUERY = gql`
-  query {
-    popularMovies {
-      results {
-        id
-        title
-        overview
-        poster_path
-      }
-    }
-  }
-`
+// TODO
